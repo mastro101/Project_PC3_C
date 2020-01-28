9f47cb73b25a439d7d05e0fe977d01af26d8adf7
Denis <serradenis2@live.it>
2020-01-28 16:32:14 +0100

    Add new scene - Folder: "SceneTempCinemachine"

---
+++ Assets/Scenes/SceneTempCinemachine/Room.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    public GameObject virtualCamera;
+
+    public virtual void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !other.isTrigger)
+        {
+            virtualCamera.SetActive(true);
+        }
+
+    }
+
+    public virtual void OntriggerExit(Collider other)
+{
+
+    if (other.CompareTag("Player") && !other.isTrigger)
+    {
+        virtualCamera.SetActive(false);
+    }
+    
+
+
+}
+}
+
+
+
+
