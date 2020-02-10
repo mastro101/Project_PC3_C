26bcc96b050137ca23818b63b61381143fb1991a
WilliamLacalamita <william.lacalamita@gmail.com>
2020-02-10 19:47:29 +0100

    Added spinning start around the player

---
+++ Assets/VFX/SpinnerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinnerController : MonoBehaviour
+{
+    ParticleSystem ps;
+    ParticleSystem.VelocityOverLifetimeModule vel;
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+        vel = ps.velocityOverLifetime;
+        vel.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            vel.orbitalY = new ParticleSystem.MinMaxCurve(2.5f);
+        }else if (Input.GetAxis("Horizontal") < 0)
+        {
+            vel.orbitalY = new ParticleSystem.MinMaxCurve(-2.5f);
+        }
+    }
+}
